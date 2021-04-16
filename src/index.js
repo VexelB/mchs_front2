2682a9c370364ac524b755788ede3e1e6c3239c7
Voytin Evgeny <vexelb@gmail.com>
2021-04-16 14:39:35 +0300

    init blin

--- src/index.js
+++ src/index.js
@@ -4,10 +4,62 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import logo from './logo.svg';
 
+class MenuItem extends React.Component {
+  render() {
+    return <li className="tables">
+      <div 
+        className="btns" 
+        onClick={() => {alert(`click ${this.props.value[0]}`)}}  
+        id={this.props.value[0]}
+      >
+          {this.props.value[1]}
+      </div>
+    </li>
+  }
+}
+
+class Menu extends React.Component {
+  renderMenuItems(a, b){
+    return <MenuItem value = {[a,b]}/>
+  }
+  render() {
+    return (
+      <ul className="submenu">
+        {this.renderMenuItems("people", "Личный состав")}
+        {this.renderMenuItems("access", "Пользовательский доступ")}
+      </ul>
+    );
+  }
+}
+
+class Header extends React.Component {
+  renderMenu(a){
+    return <Menu value={a} />;
+  }
+  render() {
+    return (
+      <div id = "header">
+        <div id="menu">
+            <div id = "tables" className = "btns">Таблицы
+                {this.renderMenu('mda')}
+            </div>
+            <div id = "shapbtns">
+                <button id ="addbtn">Добавить</button>
+            </div>
+        </div>
+    </div>
+    );
+  }
+}
+
 class App extends React.Component {
+  renderHeader() {
+    return <Header />;
+  }
   render() {
     return (
       <div className="App">
+        {this.renderHeader()}
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
